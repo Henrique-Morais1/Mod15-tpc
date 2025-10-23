44ade3d655bb32f93859369ebd3a293e1f596f9b
Francisco <31396@esjaloures.org>
2025-10-23 12:40:49 +0100

    sla

--- Formula1_system/4_f1_pilotos.sql
+++ Formula1_system/4_f1_pilotos.sql
@@ -4,22 +4,22 @@ DROP Table pilotos;
 
 CREATE TABLE pilotos (
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_equipe INT(11) UNSIGNED NOT NULL,
+    id_equipa INT(11) UNSIGNED NOT NULL,
     nome VARCHAR(50) NOT NULL,
     nacionalidade VARCHAR(50) NOT NULL,
     data_de_nascimento DATE NOT NULL,
     numero_de_campeonatos_ganhos INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_equipe) REFERENCES equipas(id)
+    FOREIGN KEY (id_equipa) REFERENCES equipas(id)
 );
 
-INSERT INTO pilotos (id_equipe, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
+INSERT INTO pilotos (id_equipa, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
 VALUES(1, 'Lewis Hamilton', 'Reino Unido', '1985-01-07', 7);
 
-INSERT INTO pilotos (id_equipe, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
+INSERT INTO pilotos (id_equipa, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
 VALUES(2, 'Max Verstappen', 'Países Baixos', '1997-09-30', 2);
 
-INSERT INTO pilotos (id_equipe, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
+INSERT INTO pilotos (id_equipa, nome, nacionalidade, data_de_nascimento, numero_de_campeonatos_ganhos)
 VALUES(3, 'Charles Leclerc', 'Mónaco', '1997-10-16', 0);
 
 SELECT * FROM pilotos;
